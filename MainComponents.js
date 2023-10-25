55ce8b638468cdac4f95d6a0341e35da359744de
tratauero <s22110430@student.unklab.ac.id>
2023-10-25 21:25:11 +0800

    Kedua

---
+++ MainComponents.js
@@ -0,0 +1,31 @@
+// Membuat objek
+const Link = ReactRouterDOM.Link;
+const Route = ReactRouterDOM.Route;
+
+// Create main class
+class App extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <ReactRouterDOM.HashRouter>
+                <div className="App">
+                    <ul className="App-header">
+                        <li> <Link to ="/">Home</Link></li>
+                        <li> <Link to ="/about">About Us</Link></li>
+                        <li> <Link to ="/contact">Contact Us</Link></li>
+                    </ul>
+
+                    <Route path="/" exact component={Home} />
+                    <Route path="/about" component={About} />
+                    <Route path="/contact" component={Contact} />
+                </div>
+            </ReactRouterDOM.HashRouter>
+        );
+    }
+}
+
+// Render komponen ke RealDOM
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ components/Home.js
@@ -0,0 +1,9 @@
+// default page
+function Home (){
+    return (
+        <div>
+            <h1>Welcome to the Class Front End !!!</h1>
+            <h3>How are you today?</h3>
+        </div>
+    );
+}
